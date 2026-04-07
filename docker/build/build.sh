02e0b99cc38afb9fc8bc8979775aefb33c22cc16
PreetP724 <patelpreet2004@gmail.com>
2026-04-07 15:52:41 -0400

    no push

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -1,4 +1,4 @@
 docker build -t preetp724/app_backend:latest -f backend.dockerfile ../..
 docker build -t preetp724/app_frontend:latest -f frontend.dockerfile ../..
-docker push preetp724/app_backend:latest
-docker push preetp724/app_frontend:latest
+#docker push preetp724/app_backend:latest
+#docker push preetp724/app_frontend:latest
